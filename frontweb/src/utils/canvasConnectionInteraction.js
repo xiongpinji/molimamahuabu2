ceb502a8309340845be446806e96e3290b5406dd
canqu <340477530@qq.com>
2026-07-29 16:00:38 +0800

    feat(画布): 扩大连线吸附命中范围

---
+++ frontweb/src/utils/canvasConnectionInteraction.js
@@ -0,0 +1,8 @@
+const DEFAULT_CONNECTION_RADIUS = 20
+const DEFAULT_EDGE_UPDATER_RADIUS = 10
+
+export const canvasConnectionInteractionOptions = Object.freeze({
+  connectionRadius: DEFAULT_CONNECTION_RADIUS * 2,
+  edgeUpdaterRadius: DEFAULT_EDGE_UPDATER_RADIUS * 2,
+  connectOnClick: true,
+})
