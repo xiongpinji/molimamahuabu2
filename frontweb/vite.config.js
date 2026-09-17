baed82ca27d142350bf78fdcea0fe39cdf771b2a
Xiongpinji <340477530@qq.com>
2026-09-18 06:18:11 +0800

    fix(frontweb): prebundle three/threepipe in Vite optimizeDeps

--- frontweb/vite.config.js
+++ frontweb/vite.config.js
@@ -11,6 +11,9 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
   },
+  optimizeDeps: {
+    include: ['three', 'threepipe'],
+  },
   server: {
     host: '0.0.0.0',
     port: 3013,
